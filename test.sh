56615b550690b08aadb247d95fbe316784c6e08d
Kai Davenport <kaiyadavenport@gmail.com>
2015-06-04 14:52:52 +0100

    use private/public format for cluster.yml

--- test.sh
+++ test.sh
@@ -29,9 +29,9 @@ cd $DIR/unofficial-flocker-tools
 cat << EOF > $DIR/unofficial-flocker-tools/cluster.yml
 cluster_name: flockerdemo
 agent_nodes:
- - $master
- - $runner1
- - $runner2
+ - {public: $master, private: $master}
+ - {public: $runner1, private: $runner1}
+ - {public: $runner2, private: $runner2}
 control_node: $master
 users:
  - flockerdemo
