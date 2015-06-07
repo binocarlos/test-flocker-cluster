d0d41cfef47973b9a24e0eafa62e1613c7286556
Kai Davenport <kaiyadavenport@gmail.com>
2015-06-07 12:48:39 +0100

    get the files downloaded into a configurable folder - if the folder exists then exit

--- boot.sh
+++ boot.sh
@@ -2,17 +2,24 @@
 
 export BOX_URL=${BOX_URL:=http://build.clusterhq.com/results/vagrant/master/flocker-tutorial.json}
 export BASE_URL=${BASE_URL:=http://doc-dev.clusterhq.com}
-
+export DOWNLOAD_FOLDER=${DOWNLOAD_FOLDER:=_files}
 #vagrant box add $BOX_URL
 
-rm -rf _tutorial
-mkdir -p _tutorial
+if [[ -f "$DOWNLOAD_FOLDER/Vagrantfile" ]]; then
+    >&2 echo "Existing installation found: $DOWNLOAD_FOLDER"
+    >&2 echo "vagrant destroy - remove that folder and try again"
+    exit 1
+fi
 
-# we just 
+rm -rf $DOWNLOAD_FOLDER
+mkdir -p $DOWNLOAD_FOLDER
+cd $DOWNLOAD_FOLDER
+
+# remove the box_url and box_version lines from the Vagrantfile 
 curl -s ${BASE_URL}/_downloads/Vagrantfile \
     | grep -vE 'config\.vm\.box_url|config\.vm\.box_version' > Vagrantfile
 
-curl -O ${BASE_URL}/_downloads/Vagrantfile
-curl -O ${BASE_URL}/_downloads/cluster.crt
-curl -O ${BASE_URL}/_downloads/user.crt
-curl -O ${BASE_URL}/_downloads/user.key
+# download the certs
+curl -sO ${BASE_URL}/_downloads/cluster.crt
+curl -sO ${BASE_URL}/_downloads/user.crt
+curl -sO ${BASE_URL}/_downloads/user.key
