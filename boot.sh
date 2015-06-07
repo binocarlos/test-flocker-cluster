f8c6e940aaa2d8797cac58516e32c6de2933e337
Kai Davenport <kaiyadavenport@gmail.com>
2015-06-07 15:34:49 +0100

    vagrant setup from flocker aster

--- boot.sh
+++ boot.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 export BOX_URL=${BOX_URL:=http://build.clusterhq.com/results/vagrant/master/flocker-tutorial.json}
 export BASE_URL=${BASE_URL:=http://doc-dev.clusterhq.com}
 export DOWNLOAD_FOLDER=${DOWNLOAD_FOLDER:=_files}
@@ -13,7 +15,8 @@ if [[ -f "$DOWNLOAD_FOLDER/Vagrantfile" ]]; then
 fi
 
 # get the box added
-vagrant box add $BOX_URL
+# this fails if the box is already added
+vagrant box add $BOX_URL 2>/dev/null || true
 
 # create and cd to the download folder
 mkdir -p $DOWNLOAD_FOLDER
@@ -28,3 +31,13 @@ curl -s ${BASE_URL}/_downloads/Vagrantfile \
 curl -sO ${BASE_URL}/_downloads/cluster.crt
 curl -sO ${BASE_URL}/_downloads/user.crt
 curl -sO ${BASE_URL}/_downloads/user.key
+
+vagrant up
+
+cat <<EOT
+curl -s \\
+    --cacert $$DOWNLOAD_FOLDER/cluster.crt \\
+    --cert $$DOWNLOAD_FOLDER/user.crt \\
+    --key $DOWNLOAD_FOLDER/user.key \\
+    https://172.16.255.250:4523/v1/state/nodes
+EOT
